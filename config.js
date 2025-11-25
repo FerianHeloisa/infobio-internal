24e3ffdb68a075353b19cc058eafa78dd6c9f978
FerianHeloisa <heloisaferian@gmail.com>
2025-11-25 20:36:15 -0300

    config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzNC9AQMAaoaAayiQ8XuWGP1hgKruJY47mryn1JxybdkeMZjS3Lzepir6HtZEMTvFrJ3w/exec"
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxgP26hYGXNqSGZYHb2YmjooLXjIVRg-Eu4FilaLzhuMyh0gI7wzQEVQYiBKkAQlDlifg/exec"
 };
